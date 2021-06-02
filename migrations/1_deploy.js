89fd25093c9810aab698a5a4d4bc3d14ef914066
jimao <1982775886@qq.com>
2021-06-02 17:02:55 +0800

    部署脚本

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,5 @@
 const UniswapV2Factory = artifacts.require('UniswapV2Factory');
 
 module.exports = (deployer) => {
-  deployer.deploy(UniswapV2Factory, '');
+  deployer.deploy(UniswapV2Factory, '0x6016C259F7c47052bfe88835c0d9817958AeeA26');
 };
