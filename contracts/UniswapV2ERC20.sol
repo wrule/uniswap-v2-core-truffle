10e9d2ffa6a39831312fb47b678179ec89ea2729
jimao <1982775886@qq.com>
2021-06-11 23:18:25 +0800

    符合uniswap的erc20代币标准实现

--- contracts/UniswapV2ERC20.sol
+++ contracts/UniswapV2ERC20.sol
@@ -3,6 +3,7 @@ pragma solidity =0.5.16;
 import './interfaces/IUniswapV2ERC20.sol';
 import './libraries/SafeMath.sol';
 
+// 符合uniswap的erc20代币标准实现
 contract UniswapV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
