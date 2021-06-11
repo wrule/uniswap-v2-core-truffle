ddfe21295f8f359e9d7fee812acbbf6207b147cd
jimao <1982775886@qq.com>
2021-06-11 23:28:36 +0800

    更换新的部署地址

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,5 @@
 const UniswapV2Factory = artifacts.require('UniswapV2Factory');
 
 module.exports = (deployer) => {
-  deployer.deploy(UniswapV2Factory, '0x6016C259F7c47052bfe88835c0d9817958AeeA26');
+  deployer.deploy(UniswapV2Factory, '0xd9B806613d73326643f51210249B1DE645751768');
 };
